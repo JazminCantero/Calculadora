b1791ab6e926b47bcd421818ab3fd8747a450442
JazminCantero <jaz.abi.gonza16@gmail.com>
2025-06-05 07:08:24 -0300

    Update Calculado.py

--- Calculado.py
+++ Calculado.py
@@ -11,3 +11,9 @@ def dividir(a, b):
     if b == 0:
         return "Error: división por cero"
     return a / b
+import math
+
+def raiz_cuadrada(x):
+    if x < 0:
+        return "Error: raíz de número negativo"
+    return math.sqrt(x)
